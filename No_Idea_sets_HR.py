438354d237123c7f54323a55470607a5e530c791
zarana-nakrani <61610030+zarana-nakrani@users.noreply.github.com>
2022-01-17 23:17:52 +0530

    Update No_Idea_sets_HR.py
    understandable variable names

--- No_Idea_sets_HR.py
+++ No_Idea_sets_HR.py
@@ -1,15 +1,15 @@
 #import array as arr
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def No_Idea(lst,a,b):
-    hpns = 0
+    happiness = 0 
     for i in lst:
         if i in a:
-            hpns+=1
+            happiness+=1
         elif i in b:
-            hpns-=1
+            happiness-=1
         else:
             pass
-    return hpns
+    return happiness
 #reading input
 if __name__ == "__main__":
     n,m = input().split(" ")
